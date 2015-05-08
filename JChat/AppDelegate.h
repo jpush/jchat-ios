4182b37c8db640070bc58940a0023a9e5836f24f
Javen <javen@jpush.cn>
2015-05-08 13:23:21 +0800

    Add third party log framework.
    Former-commit-id: 68baa2c01d7bed02bdd50409cf264a86b250919b

--- JChat/AppDelegate.h
+++ JChat/AppDelegate.h
@@ -4,11 +4,14 @@
 
 #define UMENG_APPKEY @"55487cee67e58e5431003b06"
 
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (nonatomic,strong) JPIMTabBarViewController *tabBarCtl;
 
 @property (strong, nonatomic) UIWindow *window;
+
+
 
 @end
 
---
+++ JChat/External/JCHATCustomFormatter.h
@@ -0,0 +1,12 @@
+//
+// Created by Javen on 15/5/8.
+// Copyright (c) 2015 HXHG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DDLog.h"
+
+
+@interface JCHATCustomFormatter : NSObject <DDLogFormatter>
+
+@end
