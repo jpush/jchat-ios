7ffb6fe5b546cf72e72ed7660d81c4269444ac19
Javen <javen@jpush.cn>
2015-05-08 13:23:21 +0800

    Add third party log framework.

--- JChat/AppDelegate.m
+++ JChat/AppDelegate.m
@@ -1,42 +1,28 @@
 #import "AppDelegate.h"
-#import "JPIMCommon.h"
 #import "JPIMLoginViewController.h"
 #import "JPIMChatViewController.h"
 #import "JPIMContactsViewController.h"
 #import "JPIMUserInfoViewController.h"
 #import "JPIMFileManager.h"
 #import "MobClick.h"
+#import "JCHATCustomFormatter.h"
+#import "JPIMCommon.h"
 #import <JMessage/JMessage.h>
+#import <CocoaLumberjack/CocoaLumberjack.h>
+#import <CocoaLumberjack/DDLegacyMacros.h>
 
 @implementation AppDelegate
 
 
-- (void)umengTrack {
-    //    [MobClick setCrashReportEnabled:NO]; // 如果不需要捕捉异常，注释掉此行
-    [MobClick setLogEnabled:YES];  // 打开友盟sdk调试，注意Release发布时需要注释掉此行,减少io消耗
-    [MobClick setAppVersion:XcodeAppVersion]; //参数为NSString * 类型,自定义app版本信息，如果不设置，默认从CFBundleVersion里取
-    //
-    [MobClick startWithAppkey:UMENG_APPKEY reportPolicy:(ReportPolicy) REALTIME channelId:nil];
-    //   reportPolicy为枚举类型,可以为 REALTIME, BATCH,SENDDAILY,SENDWIFIONLY几种
-    //   channelId 为NSString * 类型，channelId 为nil或@""时,默认会被被当作@"App Store"渠道
-    
-    //      [MobClick checkUpdate];   //自动更新检查, 如果需要自定义更新请使用下面的方法,需要接收一个(NSDictionary *)appInfo的参数
-    //    [MobClick checkUpdateWithDelegate:self selector:@selector(updateMethod:)];
-    
-    [MobClick updateOnlineConfig];  //在线参数配置
-    
-    //    1.6.8之前的初始化方法
-    //    [MobClick setDelegate:self reportPolicy:REALTIME];  //建议使用新方法
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onlineConfigCallBack:) name:UMOnlineConfigDidFinishedNotification object:nil];
-}
-
-- (void)onlineConfigCallBack:(NSNotification *)note {
-    NSLog(@"online config has fininshed and note = %@", note.userInfo);
-}
 
 - (BOOL)application:(UIApplication *)application
         didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self initLogger];
+
+    DDLogInfo(@"Action - didFinishLaunchingWithOptions");
+
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
     [self.window makeKeyAndVisible];
     [self umengTrack];
     [JMessage setupJMessage:launchOptions
@@ -76,13 +62,31 @@
     return YES;
 }
 
+- (void)initLogger {
+  [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+//  [[DDTTYLogger sharedInstance] setForegroundColor:RGBCOLOR(0, 0, 255)
+//                                   backgroundColor:nil
+//                                           forFlag:LOG_FLAG_INFO];
+
+  JCHATCustomFormatter *formatter = [[JCHATCustomFormatter alloc] init];
+
+    // XCode console
+  [[DDTTYLogger sharedInstance] setLogFormatter:formatter];
+  [DDLog addLogger:[DDTTYLogger sharedInstance]];
+
+  DDFileLogger *fileLogger = [[DDFileLogger alloc] init];
+  fileLogger.rollingFrequency = 60 * 60 * 24; // 一个LogFile的有效期长，有效期内Log都会写入该LogFile
+  fileLogger.logFileManager.maximumNumberOfLogFiles = 7;//最多LogFile的数量
+  [fileLogger setLogFormatter:formatter];
+  [DDLog addLogger:fileLogger];
+}
+
 - (void)networkDidSetup:(NSNotification *)notification {
-    JPIMLog(@"networkDidSetup");
+  DDLogDebug(@"Action - networkDidSetup");
 }
 
 - (void)networkDidClose:(NSNotification *)notification {
-    JPIMLog(@"networkDidClose");
-
+  JPIMLog(@"networkDidClose");
 }
 
 - (void)networkDidRegister:(NSNotification *)notification {
@@ -303,6 +307,30 @@ forLocalNotification:(UILocalNotification *)notification
     self.tabBarCtl.viewControllers = [NSArray arrayWithObjects:chatNav,contactsNav,settingNav,nil];
 }
 
+
+
+- (void)umengTrack {
+    //    [MobClick setCrashReportEnabled:NO]; // 如果不需要捕捉异常，注释掉此行
+    [MobClick setLogEnabled:YES];  // 打开友盟sdk调试，注意Release发布时需要注释掉此行,减少io消耗
+    [MobClick setAppVersion:XcodeAppVersion]; //参数为NSString * 类型,自定义app版本信息，如果不设置，默认从CFBundleVersion里取
+    //
+    [MobClick startWithAppkey:UMENG_APPKEY reportPolicy:(ReportPolicy) REALTIME channelId:nil];
+    //   reportPolicy为枚举类型,可以为 REALTIME, BATCH,SENDDAILY,SENDWIFIONLY几种
+    //   channelId 为NSString * 类型，channelId 为nil或@""时,默认会被被当作@"App Store"渠道
+
+    //      [MobClick checkUpdate];   //自动更新检查, 如果需要自定义更新请使用下面的方法,需要接收一个(NSDictionary *)appInfo的参数
+    //    [MobClick checkUpdateWithDelegate:self selector:@selector(updateMethod:)];
+
+    [MobClick updateOnlineConfig];  //在线参数配置
+
+    //    1.6.8之前的初始化方法
+    //    [MobClick setDelegate:self reportPolicy:REALTIME];  //建议使用新方法
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onlineConfigCallBack:) name:UMOnlineConfigDidFinishedNotification object:nil];
+}
+
+- (void)onlineConfigCallBack:(NSNotification *)note {
+    NSLog(@"online config has fininshed and note = %@", note.userInfo);
+}
 
 
 @end
