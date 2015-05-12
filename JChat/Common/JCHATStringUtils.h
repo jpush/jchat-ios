dce4ce659ec35874233769da28e11942f50606ec
Javen <javen@jpush.cn>
2015-05-12 16:01:38 +0800

    Refine code format.
    Former-commit-id: 2aebd3ba19c2d7bf274260aa1f637a742cfc6808

---
+++ JChat/Common/JCHATStringUtils.h
@@ -0,0 +1,14 @@
+//
+// Created by Javen on 15/5/11.
+// Copyright (c) 2015 HXHG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface JCHATStringUtils : NSObject
+
+
++ (NSString *)dictionary2String:(NSDictionary *)dictionary;
+
+@end
