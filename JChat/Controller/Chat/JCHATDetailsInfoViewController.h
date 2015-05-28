e39176d3d97c80006906e7b7dc2ed381d60afe72
guanjingfen <guanjf@jpush.cn>
2015-05-28 15:51:15 +0800

    提交单聊创建群聊UI逻辑

--- JChat/Controller/Chat/JCHATDetailsInfoViewController.h
+++ JChat/Controller/Chat/JCHATDetailsInfoViewController.h
@@ -9,10 +9,12 @@
 #import <UIKit/UIKit.h>
 #import "JCHATDetailTableViewCell.h"
 #import "JCHATChatModel.h"
+#import "JCHATSendMessageViewController.h"
 #import <JMessage/JMessage.h>
 
-@interface JCHATDetailsInfoViewController : UIViewController
+@interface JCHATDetailsInfoViewController : UIViewController<UIAlertViewDelegate>
 @property (nonatomic,strong) JMSGUser *chatUser;
 @property (nonatomic,strong) JMSGConversation *conversation;
+@property (nonatomic,strong) JCHATSendMessageViewController *sendMessageCtl;
 
 @end
