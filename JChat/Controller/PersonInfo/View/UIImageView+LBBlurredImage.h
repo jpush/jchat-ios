badfa6c69eb401d9d78e2cb14435936a3416326d
huangminlinux <380108184@qq.com>
2015-07-28 16:40:15 +0800

    修改了，设置界面，头像展示，很部分图片修改

---
+++ JChat/Controller/PersonInfo/View/UIImageView+LBBlurredImage.h
@@ -0,0 +1,36 @@
+//
+//  UIImageView+LBBlurredImage.h
+//  LBBlurredImage
+//
+//  Created by Luca Bernardi on 11/11/12.
+//  Copyright (c) 2012 Luca Bernardi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void(^LBBlurredImageCompletionBlock)(NSError *error);
+
+extern NSString *const kLBBlurredImageErrorDomain;
+
+extern CGFloat   const kLBBlurredImageDefaultBlurRadius;
+
+enum LBBlurredImageError {
+    LBBlurredImageErrorFilterNotAvailable = 0,
+};
+
+
+@interface UIImageView (LBBlurredImage)
+
+/**
+ Set the blurred version of the provided image to the UIImageView
+ 
+ @param UIImage the image to blur and set as UIImageView's image
+ @param CGFLoat the radius of the blur used by the Gaussian filter
+ *param LBBlurredImageCompletionBlock a completion block called after the image
+    was blurred and set to the UIImageView (the block is dispatched on main thread)
+ */
+- (void)setImageToBlur: (UIImage *)image
+            blurRadius: (CGFloat)blurRadius
+       completionBlock: (LBBlurredImageCompletionBlock) completion;
+
+@end
