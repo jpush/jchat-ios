9891827cb51b0594b26be5d909b81834dbe0ce3f
huangminlinux <380108184@qq.com>
2015-08-14 09:28:43 +0800

    1.添加了第三方库 GPUImage  2. 提示了头像模糊效率

--- JChat/Controller/PersonInfo/View/JCHATAvatarView.m
+++ JChat/Controller/PersonInfo/View/JCHATAvatarView.m
@@ -9,6 +9,8 @@
 #import "JCHATAvatarView.h"
 #import <JMessage/JMessage.h>
 #import "JChatConstants.h"
+#import <Accelerate/Accelerate.h>
+#import "Masonry.h"
 @implementation JCHATAvatarView
 
 /*
@@ -39,7 +41,20 @@
   self = [super initWithFrame:frame];
   if (self) {
 
-    
+//    imageView = [[GPUImageView alloc] initWithFrame:frame];
+    imageView = [GPUImageView new];
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    imageView.fillMode = UIViewContentModeScaleAspectFill;
+    imageView.clipsToBounds = YES;
+    [self addSubview:imageView];
+    [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+      make.right.mas_equalTo(self);
+      make.left.mas_equalTo(self);
+      make.top.mas_equalTo(self);
+      make.bottom.mas_equalTo(self);
+    }];
+
+ 
     self.centeraverter = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 70, 70)];
     self.centeraverter.layer.cornerRadius = 35;
     self.centeraverter.layer.masksToBounds = YES;
@@ -73,12 +88,36 @@
 
 
 - (void)setOriginImage:(UIImage *)originImage{
-  [self setImageToBlur:originImage blurRadius:kLBBlurredImageDefaultBlurRadius completionBlock:^(NSError *error) {
-    NSLog(@"blur done");
-  }];
+  UIImage *inputImage = originImage; // The WID.jpg example is greater than 2048 pixels tall, so it fails on older devices
+  sourcePicture = [[GPUImagePicture alloc] initWithImage:inputImage smoothlyScaleOutput:YES];
+  sepiaFilter = [[GPUImageTiltShiftFilter alloc] init];
+  
+  //    sepiaFilter = [[GPUImageSobelEdgeDetectionFilter alloc] init];
+  
+//  GPUImageView *imageView = (GPUImageView *)imageView;
+  
+  [sepiaFilter forceProcessingAtSize:imageView.sizeInPixels]; // This is now needed to make the filter run at the smaller output size  sizeInPixels
+  [(GPUImageTiltShiftFilter *)sepiaFilter setTopFocusLevel:2];
+  [(GPUImageTiltShiftFilter *)sepiaFilter setBottomFocusLevel:2];
+
+
+  [sourcePicture addTarget:sepiaFilter];
+  [sepiaFilter addTarget:imageView];
+
+
+
+
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    [sourcePicture processImage];
+  });
   
   self.centeraverter.image = originImage;
   
 }
+
+
+
+
+
 
 @end
